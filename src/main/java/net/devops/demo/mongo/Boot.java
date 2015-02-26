4ff7b312626e0e51e8c6da47ab3f486a3460d247
Kamil Kiewisz <kamkie@outlook.com>
2015-02-26 08:44:19 +0100

    add logs, start working on mongo custom converters

--- src/main/java/net/devops/demo/mongo/Boot.java
+++ src/main/java/net/devops/demo/mongo/Boot.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@EnableMongoRepositories
 public class Boot {
 
     public static void main(String[] args) {
--- src/main/java/net/devops/demo/mongo/Controller.java
+++ src/main/java/net/devops/demo/mongo/Controller.java
@@ -1,52 +1,114 @@
 package net.devops.demo.mongo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.List;
 import java.util.stream.LongStream;
 
+@Slf4j
 @RestController
+@RequestMapping("/mongo")
 public class Controller {
 
     @Autowired
     ModelRepository modelRepository;
 
-    @RequestMapping("/findAll")
-    public List<Model> findAll() {
-        return modelRepository.findAll();
+    @SuppressWarnings("SpringJavaAutowiringInspection")
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    private String collectionName;
+
+    @PostConstruct
+    void init() {
+        collectionName = mongoTemplate.getCollectionName(Model.class);
+    }
+
+    @RequestMapping("/list")
+    public ResponseEntity list() {
+        Instant start = Instant.now();
+        List<Model> list = modelRepository.findAll();
+
+        Instant stop = Instant.now();
+        Duration duration = Duration.between(start, stop);
+        log.info("list took {}", duration);
+        return ResponseEntity.ok(list);
+    }
+
+    @RequestMapping("/count")
+    public ResponseEntity count() {
+        Instant start = Instant.now();
+        long count = mongoTemplate.getCollection(collectionName).count();
+
+        Instant stop = Instant.now();
+        Duration duration = Duration.between(start, stop);
+        log.info("caunt took {}", duration);
+        return ResponseEntity.ok(count);
     }
 
     @RequestMapping("/create")
-    public List<Model> create() {
-        LongStream.range(0, 1000).parallel().forEach(i -> {
+    public ResponseEntity create(@RequestParam(defaultValue = "1000", value = "limit") long limit) {
+        Instant start = Instant.now();
+        LongStream.range(0, limit).parallel().forEach(i -> {
             Model model = Model.builder()
                     .firstName("first" + i)
                     .secondName("second" + i)
                     .lastName("last" + i)
-                    .socialNumber(i * 1000)
+                    .socialNumber(i * 100)
+                    .created(LocalDateTime.now(ZoneOffset.UTC))
+                    .modified(LocalDateTime.now(ZoneOffset.UTC))
                     .build();
 
             modelRepository.save(model);
         });
 
-        return modelRepository.findAll();
+        Instant stop = Instant.now();
+        Duration duration = Duration.between(start, stop);
+        log.info("create took {}", duration);
+        return ResponseEntity.ok(mongoTemplate.getCollection(collectionName).count());
     }
 
     @RequestMapping("/update")
-    public List<Model> update() {
+    public ResponseEntity update() {
+        Instant start = Instant.now();
         List<Model> models = modelRepository.findAll();
 
         models.stream().forEach(model -> {
-            model.setFirstName("first" + "uhbed");
-            model.setSecondName("second" + "mnbvc");
-            model.setLastName("last" + "adsad");
-            model.setSocialNumber(model.getSocialNumber() + 1);
+            Long socialNumber = model.getSocialNumber();
+            model.setFirstName("first" + socialNumber + "uhbed");
+            model.setSecondName("second" + socialNumber + "mnbvc");
+            model.setLastName("last" + socialNumber + "adsad");
+            model.setSocialNumber(socialNumber + 1);
+            model.setModified(LocalDateTime.now(ZoneOffset.UTC));
 
             modelRepository.save(model);
         });
 
-        return models;
+        Instant stop = Instant.now();
+        Duration duration = Duration.between(start, stop);
+        log.info("update took {}", duration);
+        return ResponseEntity.ok(mongoTemplate.getCollection(collectionName).getStats().ok());
+    }
+
+    @RequestMapping("/delete")
+    public ResponseEntity delete() {
+        Instant start = Instant.now();
+        modelRepository.deleteAll();
+
+        Instant stop = Instant.now();
+        Duration duration = Duration.between(start, stop);
+        log.info("delete took {}", duration);
+        return ResponseEntity.ok(mongoTemplate.getCollection(collectionName).getStats().ok());
     }
 }
