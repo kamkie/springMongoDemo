efd4db3b055128f874e550764bd834623a0b91de
Kamil Kiewisz <kamkie@outlook.com>
2015-02-26 09:26:36 +0100

    working mongo conversions, hardcoded configuration

--- src/main/java/net/devops/demo/mongo/MongoConfiguration.java
+++ src/main/java/net/devops/demo/mongo/MongoConfiguration.java
@@ -1,16 +1,32 @@
 package net.devops.demo.mongo;
 
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.convert.CustomConversions;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
 
 @Configuration
-public class MongoConfiguration {
+@Component
+public class MongoConfiguration extends AbstractMongoConfiguration {
 
+    @Override
+    protected String getDatabaseName() {
+        return "demo";
+    }
+
+    @Override
+    public Mongo mongo() throws Exception {
+        return new MongoClient();
+    }
+
+    @Override
     @Bean
     public CustomConversions customConversions() {
         return new CustomConversions(Arrays.asList(
@@ -19,14 +35,16 @@ public class MongoConfiguration {
         ));
     }
 
-    public class LocalDateTimeToStringConverter implements Converter<LocalDateTime, String> {
+    @Component
+    public static class LocalDateTimeToStringConverter implements Converter<LocalDateTime, String> {
         @Override
         public String convert(LocalDateTime localDate) {
             return localDate.toString();
         }
     }
 
-    public class StringToLocalDateTimeConverter implements Converter<String, LocalDateTime> {
+    @Component
+    public static class StringToLocalDateTimeConverter implements Converter<String, LocalDateTime> {
         @Override
         public LocalDateTime convert(String source) {
             return LocalDateTime.parse(source);
